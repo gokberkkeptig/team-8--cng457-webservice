0eeb25f71a6b8c05a3f49ffa21b064f97c788f67
redhatt06 <bengicana@gmail.com>
2021-05-15 20:39:47 +0300

    First commit of Brand,Comment,Product and ProductDecorator Entities. appplication.properties, conroller,repository and service are also configured.

---
+++ src/main/java/com/webServiceSpring/WebService/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.webServiceSpring.WebService.repository;
+
+import com.webServiceSpring.WebService.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
